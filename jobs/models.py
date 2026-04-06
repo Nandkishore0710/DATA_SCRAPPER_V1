d12418c771c55fa1593342ed6c87bf5af9e7b102
Nand Kishor <nandkishore2530@gmail.com>
2026-04-06 15:53:12 +0530

    Fix: Add missing cache models to fix pipeline crashing

--- jobs/models.py
+++ jobs/models.py
@@ -215,6 +215,28 @@ class ServerPressure(models.Model):
     def __str__(self):
         return f"Pressure @ {self.timestamp}: {self.active_jobs} jobs"
 
+
+class CachedBoundary(models.Model):
+    location = models.CharField(max_length=200, unique=True)
+    display_name = models.CharField(max_length=500, blank=True)
+    min_lat = models.FloatField()
+    max_lat = models.FloatField()
+    min_lng = models.FloatField()
+    max_lng = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.location
+
+
+class CachedLocationResolution(models.Model):
+    location = models.CharField(max_length=200, unique=True)
+    data = models.JSONField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.location
+
 from django.db.models.signals import pre_delete
 from django.dispatch import receiver
 from django.db import connection
